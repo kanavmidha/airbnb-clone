f3e2aab0bd498cb05357e1887c5838334831e862
Kanav Midha <kanavmiddha0001@gmail.com>
2024-10-23 01:35:36 -0600

    add search filters function. create a search modal along with a hook that allows to search properties using filters. edit get listings action to include all the params from the query. fix: fix a small issue where the back button was not working in the Modal.js file.

--- app/components/modals/Modal.js
+++ app/components/modals/Modal.js
@@ -47,7 +47,7 @@ export default function Modal({
             return
         }
 
-        secondaryAction
+        secondaryAction()
     }, [disabled, secondaryAction])
 
     if (!isOpen) {
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
 import RentModal from "./components/modals/RentModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/SearchModal";
 
 const font = Nunito({
   subsets:['latin']
@@ -24,6 +25,7 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider/>
+        <SearchModal/>
         <RegisterModal/>
         <LoginModal/>
         <RentModal/>
