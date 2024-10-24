641e40b6a5ce4556c24ef7a682e638c1917c1ee1
Kanav Midha <kanavmiddha0001@gmail.com>
2024-10-24 09:58:12 -0600

    add a force dynamic to the home page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
+export const dynamic = 'force dynamic'
 
 export default async function Home({searchParams}) {
 
